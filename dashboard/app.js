db9e3575e8052f855d295078a52a29cc6e3cb3f1
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-16 00:59:51 -0300

    fix websocket

--- dashboard/app.js
+++ dashboard/app.js
@@ -20,19 +20,30 @@ function connectDevice(){
 
         if(msg.status === "connected"){
 
+            const d = msg.data
+
             document.getElementById("status")
             .className = "status online"
 
             document.getElementById("status")
             .innerText = "ONLINE"
 
+            document.getElementById("battery")
+            .innerText = d.battery + "%"
+
+            document.getElementById("android")
+            .innerText = d.android
+
+            document.getElementById("storage")
+            .innerText = d.storage
+
+            document.getElementById("uptime")
+            .innerText = d.uptime
+
             document.getElementById("deviceInfo")
-            .innerText = id
+            .innerText = d.device_id
 
         }else{
-
-            document.getElementById("status")
-            .className = "status offline"
 
             document.getElementById("status")
             .innerText = "OFFLINE"
